ea9ddf021d05385172835d4fc9e156051385e9e0
Guilherme Avanci <guilherme.avanci@ifood.com.br>
2021-04-30 14:51:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,137 @@
+# search-json-path README
+
+Extension to find every JSON file and the respective key containing the specified path.
+
+## Motivation
+
+The creation of this extension was motivated by the usage of the [i18next](https://www.i18next.com/) library. While using i18next I felt the need to quickly access the internationalization JSON files through the path already defined inside the code. Well, that's what this extension resolve.
+
+This extension is inspired by [copy-json-path](https://marketplace.visualstudio.com/items?itemName=nidu.copy-json-path) extension, which helps you out copying the JSON key full path. Also pretty useful while using i18next.
+
+## Path format
+
+The extension currently supports simple paths. See some example below:
+
+`this.is.a.path`
+```JSON
+{
+	"this": {
+		"is": {
+			"a": {
+				"path": "yes, it is" // match this
+			}
+		}
+	}
+}
+```
+
+`take.the.second.1`
+```JSON
+{
+	"take": {
+		"the": {
+			"second": [
+				"not here",
+				"here it is" // match this
+			]
+		}
+	}
+}
+```
+
+## Features and Usage
+
+### Search Interface
+
+An input interface is invoked by the extension. Type the desired path and press `Enter`. All `.json` files inside your workspace containing the path specified must be showed in a list above the input.
+
+### Search Json Path command
+
+`Ctrl+P` and type `Search Json Path` to open the search interface. Type the desired path and press `Enter`. The [**Search Interface**](#search-interface) must appear waiting for the path insertion.
+
+
+### Right-click and use Search JSON Path
+
+Right-click in the document give you the option `Search Json Path`. It does exactly the same thing as [**Search Json Path command**](#search-json-path-command) with the exception that if some text is already selected in the document it automatically fills the [**Search Interface**](#search-interface) input.
+
+### Code Lenses
+
+The [code lens](https://code.visualstudio.com/api/language-extensions/programmatic-language-features#codelens-show-actionable-context-information-within-source-code) feature is shown inside the document directly above the line where the extension identifies a path. It's another quick way to open the [**Search Interface**](#search-interface) automatically passing the path identified.
+
+Note that the code lens feature is disabled by default to prevent undesired IDE noises because its default way to find out the paths is considering all strings a path. If you want to use this feature, the suggestion is to carefully set the `prefixAndSuffix` extension in a way to guarantee the extension can identify exactly what you want.
+
+> See more in the [**Extension Settings**](#extension-settings) section.
+
+The [**Code Lenses**](#code-lenses) supports the following documment languages:
+
+```
+javascript
+jsx
+typescript
+typescriptreact
+```
+
+## Extension Settings
+
+The extension default settings are the following:
+
+```JSON
+{
+	"search-json-path": {
+		"enable": true,
+		"prefixAndSuffix": ["'", "\""],
+		"codeLenses": {
+			"enable": false
+		}
+	}
+}
+```
+
+| Syntax | Type | Default Value | Description |
+| ----------- | ----------- | ----------- | ----------- |
+| enable | `boolean` | `true` | Define if the extension is currently active |
+| prefixAndSuffix | `String | Array<String> | Array<Array<String>>` | `["'", "\""]` | Can have many formats dependending on your needs (See more at [prefixAndSuffix formats](#prefixandsuffix-formats) section). The `String` ocurrencies can be a `Regex`. |`
+| codeLenses.enable | `boolean` | `false` | Define if the code lens feature is active |
+
+### prefixAndSuffix formats
+
+There are many ways to configure the `prefixAndSuffix` option depending on your needs. The recommendation is to look below and prioritize the simplest way which effectively finds the match you want.
+
+#### *Same* prefix and suffix
+```javascript
+"'"
+```
+> <mark>'</mark>this.is.a.path<mark>'</mark>
+
+#### *Multiple* prefix and suffix
+```javascript
+["'", "\""]
+```
+> <mark>'</mark>this.is.a.path<mark>'</mark>
+> 
+> <mark>"</mark>this.is.a.path<mark>"</mark>
+
+#### *Different* prefix and suffix
+```javascript
+[
+	["prefix-", "-suffix"]
+]
+```
+> <mark>prefix-</mark>this.is.a.path<mark>-suffix</mark>
+
+#### *Multiple and Different* prefix and suffix
+```javascript
+[
+	["prefix-", "-suffix"],
+	["t('", "')"]
+]
+```
+> <mark>prefix-</mark>this.is.a.path<mark>-suffix</mark>
+> 
+> <mark>t('</mark>this.is.a.path<mark>')</mark>
+
+## Release Notes
+
+### 0.0.1
+
+The initial release.
---
+++ extension.js
@@ -0,0 +1,283 @@
+const vscode = require("vscode");
+const pkg = require("./package.json");
+const defaultSettings = require("./defaultSettings.json");
+
+function sanitizePrefixAndSuffix(value) {
+  if (typeof value === "string") {
+    return [[value, value]];
+  }
+
+  if (Array.isArray(value)) {
+    return value.map((v) => {
+      if (typeof v === "string" || (Array.isArray(v) && v.length === 1)) {
+        return [v, v];
+      }
+      return v;
+    });
+  }
+}
+const prefixAndSuffix = sanitizePrefixAndSuffix(
+  vscode.workspace
+    .getConfiguration(pkg.name)
+    .get("prefixAndSuffix", defaultSettings.prefixAndSuffix)
+);
+
+const pattern = getEspacedPrefixAndSuffix()
+  .map((v) => `(?:${v[0]}.*?${v[1]})`)
+  .join("|");
+const regex = new RegExp(pattern, "g");
+
+let inputBox, quickPick;
+
+function search(path) {
+  let pathArray = path.split(".");
+  vscode.workspace.findFiles("**/*.json", "**/node_modules/").then((files) => {
+    Promise.all(files.map((file) => searchPathInJson(pathArray, file))).then(
+      (filesFound) =>
+        showQuickPick(
+          path,
+          filesFound.filter((fileFound) => fileFound)
+        )
+    );
+  });
+}
+
+function showQuickPick(path, filesFound) {
+  const lastKey = path.split(".").slice(-1)[0];
+  quickPick = vscode.window.createQuickPick();
+  quickPick.canSelectMany = false;
+  quickPick.title = "Insert the desired JSON path";
+  if (filesFound) {
+    quickPick.canSelectMany = filesFound.length > 1;
+    quickPick.title = `${filesFound.length} file(s) found containing the path: ${path}`;
+    quickPick.items = filesFound
+      .filter((fileFound) => fileFound)
+      .map((fileFound) => ({
+        label: fileFound.json.path.split("/").slice(-1)[0],
+        description: fileFound.json.path,
+        file: fileFound.json,
+      }));
+  }
+  quickPick.buttons = [
+    {
+      iconPath: new vscode.ThemeIcon("arrow-left"),
+      tooltip: "Back to insert a new path",
+    },
+  ];
+  quickPick.onDidAccept(() => {
+    quickPick.selectedItems.forEach((item) => {
+      const text = filesFound
+        .map((f) => {
+          if (f.json.path === item.file.path) {
+            return f.jsonString;
+          }
+          return;
+        })
+        .filter((f) => f)[0];
+
+      const changedKeys = [];
+
+      const newText = text
+        .split("\n")
+        .map((t, line) => {
+          const matches = [...t.matchAll(`"${lastKey}"`)];
+          if (matches.length) {
+            let newLine = t;
+            matches.forEach((match) => {
+              const newChangedKey = `${lastKey}_${pkg.name}_${line}_${match.index}`;
+              changedKeys.push(newChangedKey);
+              newLine = newLine.replace(`"${lastKey}"`, `"${newChangedKey}"`);
+            });
+            return newLine;
+          }
+          return t;
+        })
+        .join("\n");
+
+      const newJSON = JSON.parse(newText);
+
+      let line, column;
+      changedKeys.forEach((k) => {
+        const keyArray = path.split(".");
+        const pathWithoutLastKey =
+          keyArray.length > 1 ? `${keyArray.slice(0, -1).join(".")}.` : "";
+        if (deepFind(`${pathWithoutLastKey}${k}`, newJSON)) {
+          [line, column] = k
+            .replace(`${lastKey}_${pkg.name}_`, "")
+            .split("_")
+            .map((value) => Number(value));
+        }
+      });
+
+      vscode.window.showTextDocument(item.file, {
+        preview: false,
+        selection: new vscode.Range(
+          new vscode.Position(line, column + 1),
+          new vscode.Position(line, column + 1 + lastKey.length)
+        ),
+      });
+    });
+  });
+  quickPick.onDidTriggerButton(() => {
+    quickPick.hide();
+    inputBox.show();
+  });
+  quickPick.show();
+}
+
+function searchPathInJson(pathArray, json) {
+  return vscode.workspace.fs.readFile(json).then((content) => {
+    const jsonString = Buffer.from(content).toString();
+    const jsonObject = JSON.parse(jsonString);
+    return deepFind(pathArray, jsonObject, true) && { json, jsonString };
+  });
+}
+
+function deepFind(path, obj, getKey) {
+  let pathArray = typeof path === "string" ? path.split(".") : path,
+    current = obj,
+    next;
+  pathArray.forEach((key, i) => {
+    if (current) {
+      next = current[key];
+    }
+    if (next == undefined || next == null) {
+      current = undefined;
+      return;
+    } else if (pathArray.length - 1 === i) {
+      if (!getKey) {
+        current = next;
+        return;
+      }
+      return;
+    } else {
+      current = next;
+    }
+  });
+  return current;
+}
+
+function removePrefixAndSuffix(value) {
+  const currentPrefixAndSuffix = prefixAndSuffix.find(
+    (tuple) => value.startsWith(tuple[0]) && value.endsWith(tuple[1])
+  );
+  if (currentPrefixAndSuffix) {
+    return value.slice(
+      currentPrefixAndSuffix[0].length,
+      value.length - currentPrefixAndSuffix[1].length
+    );
+  }
+  return value;
+}
+
+function getValue() {
+  const value = vscode.window.activeTextEditor.document.getText(
+    vscode.window.activeTextEditor.selection
+  );
+  if (!value) {
+    return;
+  }
+  return removePrefixAndSuffix(value);
+}
+
+function showInput(value) {
+  inputBox = vscode.window.createInputBox();
+  inputBox.value = (typeof value === "string" && value) || getValue() || "";
+  inputBox.title = "Insert the desired JSON path";
+  inputBox.onDidAccept(() => search(inputBox.value));
+  inputBox.show();
+}
+
+function activate({ subscriptions }) {
+  if (
+    !vscode.workspace
+      .getConfiguration(pkg.name)
+      .get("enable", defaultSettings.enable)
+  ) {
+    return;
+  }
+
+  let search = vscode.commands.registerCommand(`${pkg.name}.search`, showInput);
+
+  subscriptions.push(search);
+
+  let sel = [
+    { scheme: "file", language: "javascript" },
+    { scheme: "file", language: "jsx" },
+    { scheme: "file", language: "typescript" },
+    { scheme: "file", language: "typescriptreact" },
+  ];
+  subscriptions.push(
+    vscode.languages.registerCodeLensProvider(sel, new CodeLensProvider())
+  );
+}
+
+function escapeRegex(value) {
+  return value.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
+function getEspacedPrefixAndSuffix() {
+  return prefixAndSuffix.map((tuple) =>
+    tuple.map((string) => escapeRegex(string))
+  );
+}
+
+class CodeLensProvider {
+  constructor() {
+    this.codeLenses = [];
+  }
+
+  provideCodeLenses(doc) {
+    if (
+      vscode.workspace
+        .getConfiguration(pkg.name)
+        .get("codeLenses.enable", defaultSettings.codeLenses.enable)
+    ) {
+      this.codeLenses = [];
+      const text = doc.getText();
+      let matches;
+      while ((matches = regex.exec(text)) !== null) {
+        const line = doc.lineAt(doc.positionAt(matches.index).line);
+        const indexOf = line.text.indexOf(matches[0]);
+        const position = new vscode.Position(line.lineNumber, indexOf);
+        const range = doc.getWordRangeAtPosition(position, new RegExp(regex));
+        if (range) {
+          this.codeLenses.push({
+            match: matches[0],
+            path: removePrefixAndSuffix(matches[0]),
+            codeLens: new vscode.CodeLens(range),
+          });
+        }
+      }
+      return this.codeLenses.map((c) => c.codeLens);
+    }
+    return [];
+  }
+
+  resolveCodeLens(lens) {
+    if (
+      vscode.workspace
+        .getConfiguration(pkg.name)
+        .get("codeLenses.enable", defaultSettings.codeLenses.enable)
+    ) {
+      const selectedLens = this.codeLenses.find(
+        (c) => c.codeLens.range === lens.range
+      );
+
+      lens.command = {
+        title: `Search JSON with path: ${selectedLens.path}`,
+        command: `${pkg.name}.search`,
+        arguments: [selectedLens.path],
+      };
+      return lens;
+    }
+    return null;
+  }
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
